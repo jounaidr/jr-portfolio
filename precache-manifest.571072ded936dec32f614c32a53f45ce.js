f2fac9732d8278ca4655c3862e8b7a4dd9a73028
jounaidr <jounaidruhomaun@googlemail.com>
2020-09-09 19:51:42 +0100

    Initial deployment 09/09/2020

---
+++ precache-manifest.571072ded936dec32f614c32a53f45ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "671760f5ca3d4f9007b7",
+    "url": "/static/css/main.ad696f07.chunk.css"
+  },
+  {
+    "revision": "671760f5ca3d4f9007b7",
+    "url": "/static/js/main.5f40b078.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.521e897d.chunk.js"
+  },
+  {
+    "revision": "1c744452997882a47cfeb38e577c01ac",
+    "url": "/index.html"
+  }
+];
