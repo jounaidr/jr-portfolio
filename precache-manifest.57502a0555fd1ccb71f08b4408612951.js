8e05c5b30e30898a9bc3d5db5ad1621264a7171a
jounaidr <jounaidruhomaun@googlemail.com>
2020-09-10 18:32:05 +0100

    Updates

---
+++ precache-manifest.57502a0555fd1ccb71f08b4408612951.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8bb3dfa18271d7f8860",
+    "url": "/static/css/main.9da0869f.chunk.css"
+  },
+  {
+    "revision": "b8bb3dfa18271d7f8860",
+    "url": "/static/js/main.c3b9a118.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.521e897d.chunk.js"
+  },
+  {
+    "revision": "3d614e101fe56319573a69847db35176",
+    "url": "/index.html"
+  }
+];
