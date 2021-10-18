531662988bef76e28d77a762c76d8bc3f0a57958
jounaidr <jounaidruhomaun@googlemail.com>
2021-10-18 18:33:57 +0100

    Inspirational lean quote

--- src/components/references/references.js
+++ src/components/references/references.js
@@ -16,9 +16,10 @@ export default class References extends Component {
                                         <li>
                                             <blockquote>
                                                 <p>
-                                                    ahh the pepeloni, pepeloni. you know the pepeloni? the nooo one. i always, i always order the, the domino. domino pepeloni and without pepeloni. i always order the pepeloni and without pepeloni. pepeloni! i like pepeloni, yeah. i always, i always order the, the cheese- cheese pan. ahh how can i explain? i can explain by my drawing! i always order like the cheese pan that it has cheese on here, this part, the ear. ear of pizza. and then, i order- wh- when i order pepeloni, the ear- it always have a pepeloni on h- on a top, but i pick up these... away! cause i don't eat it. and then i eat the cheese pan pizza. okay? you understand? understandable! pepeloni! yes.
+                                                    Molecules expand, molecules exist
+                                                    I'm a molecule, but I stand in the mist
                                                 </p>
-                                                <cite>Akai Haato</cite>
+                                                <cite>Yung Lean</cite>
                                             </blockquote>
                                         </li> {/* slide ends */}
                                         {/*<li>*/}
