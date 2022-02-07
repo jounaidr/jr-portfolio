cd23f0a75e7ecdc51c02a8bae408efdbf3403242
jounaidr <52418954+jounaidr@users.noreply.github.com>
2022-02-07 11:08:30 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,16 +8,17 @@ import References from './components/references/references';
 import Footer from './components/footer/footer';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <About />
-      <Resume />
-      <Portfolio />
-      <References />
-      <Footer />
-    </div>
-  );
+   return (
+//     <div className="App">
+//       <Header />
+//       <About />
+//       <Resume />
+//       <Portfolio />
+//       <References />
+//       <Footer />
+//     </div>
+     <body> doing something else on this domain, will be available soon :) </body>
+   );
 }
 
 export default App;
